8d69bd69e91c5d4fa73ab078d3ff94fe1523acdf
Pavel Kovalchuk <pavelkovalchuk0510@gmail.com>
2026-01-18 21:33:48 +0100

    Profile page layout and logic

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -6,9 +6,14 @@ namespace GymApp.Controllers
     [Authorize]
     public class ProfileController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(string section = "account", bool edit = false)
         {
+            ViewBag.Section = section;
+            ViewBag.EditMode = edit;
+
             return View();
         }
+
+
     }
 }
